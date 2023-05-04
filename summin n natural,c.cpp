0c0461fcb0801cd66e1ccd007d257c214e4dbe3b
Nikhiladi14 <112459971+Nikhiladi14@users.noreply.github.com>
2023-05-04 11:39:48 +0530

    Add files via upload

---
+++ summin n natural,c.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int i,n,sum=0;
+	printf("Enter a positive number:");
+	scanf("%d",&n);
+	for(i=0;i<=n;i++){
+		sum += i;
+	}
+	printf("sum = %d",sum);
+	return 0;
+}
